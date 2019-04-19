e65d7a2f61b359c1c27effecc83179809871b57e
amin7899 <aminmeghrazi7899@gmail.com>
2019-04-19 20:56:06 +0430

    completing shop class

--- src/view/View.java
+++ src/view/View.java
@@ -26,45 +26,48 @@ public class View {
         }
     }
 
-    public void printDeckValidation(String name){
-        System.out.println(name+" is a valid deck.");
+    public void printDeckValidation(String name) {
+        System.out.println(name + " is a valid deck.");
     }
 
-    public void printObjectId(ArrayList<Integer> id){
+    public void printObjectId(ArrayList<Integer> id) {
         for (int i = 0; i < id.size(); i++) {
             System.out.println(id.get(i));
         }
     }
 
-    public void showCollection(Collection collection){
+    public void showCollection(Collection collection) {
         int index = 1;
-        for (Hero hero: collection.getHeroes()) {
+        System.out.println("Heroes :\n\t");
+        for (Hero hero : collection.getHeroes()) {
             System.out.println(index + " : " + hero.showDetails());
             index++;
         }
         index = 1;
-        for (Item item: collection.getItems()) {
+        System.out.println("Items :\n\t");
+        for (Item item : collection.getItems()) {
             System.out.println(index + " : " + item.showDetails());
             index++;
         }
         index = 1;
-        for (Spell spell: collection.getSpells()) {
+        System.out.println("Cards :\n\t");
+        for (Spell spell : collection.getSpells()) {
             System.out.println(index + " : " + spell.showDetails());
             index++;
         }
-        for (Minion minion: collection.getMinions()) {
+        for (Minion minion : collection.getMinions()) {
             System.out.println(index + " : " + minion.showDetails());
             index++;
         }
     }
 
-    public void printDeckDetails (Deck deck){
-        System.out.println("Hereos :\n\t1 : " + deck.getHero().showDetails());
+    public void printDeckDetails(Deck deck) {
+        System.out.println("Heroes :\n\t1 : " + deck.getHero().showDetails());
         System.out.println("Items :\n\t1 : " + deck.getItem().showDetails());
         System.out.println("Cards :\n");
         int index = 1;
         for (Spell spell : deck.getSpells()) {
-            System.out.println("\t"+ index + spell.showDetails());
+            System.out.println("\t" + index + spell.showDetails());
             index++;
         }
         for (Minion minion : deck.getMinions()) {
@@ -73,7 +76,7 @@ public class View {
         }
     }
 
-    public void showCollectionMenu(){
+    public void showCollectionMenu() {
         String helpstr = "1 : Exit\n" +
                 "2 : Show\n" +
                 "3 : Search\n" +
@@ -89,4 +92,5 @@ public class View {
                 "13 : Help";
         System.out.println(helpstr);
     }
+
 }
