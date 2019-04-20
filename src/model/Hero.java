e302c235d866d3b8cbec5f3ea974cd15bc7cace9
amin7899 <aminmeghrazi7899@gmail.com>
2019-04-21 02:37:20 +0430

    Complete Class of Hero and Implement its Child

--- src/model/Hero.java
+++ src/model/Hero.java
@@ -8,7 +8,7 @@ public abstract class Hero extends Card {
     private int hp;
     private int mp;
     //SpecialPower specialPower;
-    private int typeOfRange;//0 mellee 1 ranged 2 hybrid
+    private int typeOfRange;//0 melee 1 ranged 2 hybrid
     private int range;
     private int coolDownTime;
 
@@ -30,7 +30,7 @@ public abstract class Hero extends Card {
         this.ap = hero.ap;
         this.hp = hero.hp;
         this.mp = hero.mp;
-        this.specialPower = hero.specialPower;
+        //this.specialPower = hero.specialPower;
     }
 
     public void setCoolDownTime(int coolDownTime) {
@@ -53,91 +53,270 @@ public abstract class Hero extends Card {
         return hp;
     }
 
+    public int getAp() {
+        return ap;
+    }
+
+    public String getTypeOfHit() {
+        if (this.typeOfRange == 0)
+            return "Melee";
+        else if (this.typeOfRange == 1)
+            return "Ranged";
+        return "Hybrid";
+    }
+
+    public Hero duplicate() {
+        return null;
+    }
    /* public SpecialPower getSpecialPower() {
         return specialPower;
     }
 */
 }
 
-public class WhiteBogey extends Hero {
+class WhiteBogey extends Hero {
     public WhiteBogey() {
         super("WhiteBogey", 4, 50, 8000, 0);
         super.setCoolDownTime(2);
         super.setMp(1);
     }
 
+    public WhiteBogey(WhiteBogey whiteBogey) {
+        super(whiteBogey);
+    }
+
+    public Hero duplicate() {
+        WhiteBogey whiteBogey = new WhiteBogey(this);
+        return whiteBogey;
+    }
+
+    @Override
+    public String showDetails() {
+        String details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : take a power buff that increase hit power of itself" +
+                "4 units forever.";
+        return details;
+    }
 }
 
-public class Simurgh extends Hero {
+class Simurgh extends Hero {
     public Simurgh() {
         super("Simurgh", 4, 50, 9000, 0);
         super.setCoolDownTime(8);
         super.setMp(3);
     }
+
+    public Simurgh(Simurgh simurgh) {
+        super(simurgh);
+    }
+
+    public Hero duplicate() {
+        Simurgh simurgh = new Simurgh(this);
+        return simurgh;
+    }
+
+    @Override
+    public String showDetails() {
+        String details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : 8 cells around it will be fiery for 2 turns and take 8 holy buff" +
+                " for 2 turns." +
+                "4 units forever.";
+        return details;
+    }
 }
 
-public class Dragon extends Hero {
+class Dragon extends Hero {
     public Dragon() {
         super("Dragon", 4, 50, 8000, 0);
         super.setCoolDownTime(1);
         super.setMp(0);
     }
+
+    public Dragon(Dragon dragon) {
+        super(dragon);
+    }
+
+    public Hero duplicate() {
+        Dragon dragon = new Dragon(this);
+        return dragon;
+    }
+
+    @Override
+    public String showDetails() {
+        String details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : disarm one person.";
+        return details;
+    }
 }
 
-public class Rakhsh extends Hero {
+class Rakhsh extends Hero {
     public Rakhsh() {
         super("Rakhsh", 4, 50, 8000, 0);
         super.setCoolDownTime(2);
         super.setMp(1);
     }
+
+    public Rakhsh(Rakhsh rakhsh) {
+        super(rakhsh);
+    }
+
+    public Hero duplicate() {
+        Rakhsh rakhsh = new Rakhsh(this);
+        return rakhsh;
+    }
+
+    @Override
+    public String showDetails() {
+        String details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : stun one of the enemy forces for one turn.";
+        return details;
+    }
 }
 
-public class Zahhak extends Hero {
+class Zahhak extends Hero {
     public Zahhak() {
         super("Zahhak", 4, 50, 10000, 0);
         super.setCoolDownTime(3);
         super.setMp(1);
     }
+
+    public Zahhak(Zahhak zahhak) {
+        super(zahhak);
+    }
+
+    public Hero duplicate() {
+        Zahhak zahhak = new Zahhak(this);
+        return zahhak;
+    }
+
+    @Override
+    public String showDetails() {
+        String details;
+        details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : appear two big snakes with health = 1 and hit power = 1" +
+                " in two random cells of 8 cells around itself if the cells were full it appears less than two.";
+        return details;
+    }
 }
 
-public class Kaveh extends Hero {
+class Kaveh extends Hero {
     public Kaveh() {
         super("Kaveh", 4, 50, 1, 3, , 8000, 0, );
         super.setCoolDownTime(3);
         super.setMp(1);
     }
+
+    public Kaveh(Kaveh kaveh) {
+        super(kaveh);
+    }
+
+    public Hero duplicate() {
+        Kaveh kaveh = new Kaveh(this);
+        return kaveh;
+    }
+
+    @Override
+    public String showDetails() {
+        String details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : make a cell holy for 3 turns.";
+        return details;
+    }
 }
 
-public class Arash extends Hero {
+class Arash extends Hero {
     public Arash() {
         super("Arash", 2, 30, 10000, 1);
         super.setCoolDownTime(2);
         super.setMp(2);
         super.setRange(6);
     }
+
+    public Arash(Arash arash) {
+        super(arash);
+    }
+
+    public Hero duplicate() {
+        Arash arash = new Arash(this);
+        return arash;
+    }
+
+    @Override
+    public String showDetails() {
+        String details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : hit all enemy forces in itself row with 4 units hit.";
+        return details;
+    }
 }
 
-public class Legend extends Hero {
+class Legend extends Hero {
     public Legend() {
         super("Legend", 3, 40, 11000, 1);
         super.setCoolDownTime(2);
         super.setMp(1);
         super.setRange(3);
     }
+
+    public Legend(Legend legend) {
+        super(legend);
+    }
+
+    public Hero duplicate() {
+        Legend legend = new Legend(this);
+        return legend;
+    }
+
+    @Override
+    public String showDetails() {
+        String details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : dispel one of the enemy forces.";
+        return details;
+    }
 }
 
-public class Esfandyar extends Hero {
+class Esfandyar extends Hero {
     public Esfandyar() {
         super("Esfandyar", 3, 35, 12000, 2);
         super.setCoolDownTime(1);
         super.setMp(0);
         super.setRange(3);
     }
+
+    public Esfandyar(Esfandyar esfandyar) {
+        super(esfandyar);
+    }
+
+    public Hero duplicate() {
+        Esfandyar esfandyar = new Esfandyar(this);
+        return esfandyar;
+    }
+
+    @Override
+    public String showDetails() {
+        String details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : has 3 holy buff in passive mode.";
+        return details;
+    }
 }
 
-public class Rostam extends Hero {
+class Rostam extends Hero {
     public Rostam() {
         super("Rostam", 7, 55, 8000, 2);
         super.setRange(4);
+    }
+
+    public Rostam(Rostam rostam) {
+        super(rostam);
+    }
+
+    public Hero duplicate() {
+        Rostam rostam = new Rostam(this);
+        return rostam;
+    }
+
+    @Override
+    public String showDetails() {
+        String details = "Name : " + this.getName() + " - AP : " + this.getAp() + " - HP : " + this.getHp()
+                + " - Class : " + this.getTypeOfHit() + " – Special power : - .";
+        return details;
     }
 }
--- src/model/Minion.java
+++ src/model/Minion.java
@@ -2,8 +2,8 @@ package model;
 
 import java.util.ArrayList;
 
-public abstract class Minion extends  Card{
-    private static ArrayList<Minion> minions=new ArrayList<>();
+public abstract class Minion extends Card {
+    private static ArrayList<Minion> minions = new ArrayList<>();
     private int ap;
     private int hp;
     private int costToUse;
@@ -11,6 +11,7 @@ public abstract class Minion extends  Card{
     private int typeOfRange;//0 mellee 1 ranged 2 hybrid
     private int range;
     private int timeOfActivationOfSpecialPower;//0 on attack 1 on spawn 2 combo 3 on death 4 passive 5on turn 6 on defend
+
     public int getTypeOfRange() {
         return typeOfRange;
     }
@@ -20,27 +21,28 @@ public abstract class Minion extends  Card{
     }
 
 
-    /*public Minion(String name,int ap, int hp, SpecialPower specialPower, int costOfBuy, int costToUse, int typeOfRange, int range){
+    public Minion(String name, int ap, int hp, int costOfBuy, int costToUse, int typeOfRange, int range) {
         this.setName(name);
         this.setCostOfBuy(costOfBuy);
-        this.ap=ap;
-        this.hp=hp;
-        this.specialPower=specialPower;
-        this.costToUse=costToUse;
-        this.typeOfRange=typeOfRange;
-        this.range=range;
+        this.ap = ap;
+        this.hp = hp;
+        this.costToUse = costToUse;
+        this.typeOfRange = typeOfRange;
+        this.range = range;
         minions.add(this);
-    }*/
-    public Minion(Minion minion){
+    }
+
+    public Minion(Minion minion) {
         this.setName(minion.getName());
         this.setCostOfBuy(minion.getCostOfBuy());
-        this.ap=minion.ap;
-        this.hp=minion.hp;
-        //this.specialPower=minion.specialPower;
-        this.costToUse=minion.costToUse;
-        this.typeOfRange=minion.typeOfRange;
-        this.range=minion.range;
+        this.ap = minion.ap;
+        this.hp = minion.hp;
+        this.specialPower = minion.specialPower;
+        this.costToUse = minion.costToUse;
+        this.typeOfRange = minion.typeOfRange;
+        this.range = minion.range;
     }
+
     public int getAp() {
         return ap;
     }
@@ -60,7 +62,6 @@ public abstract class Minion extends  Card{
     public static ArrayList<Minion> getMinions() {
         return minions;
     }
-
 
 
 }
